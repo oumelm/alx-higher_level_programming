04c8ff05ff27f42f4a7fd683684372f29158b0f9
oumelm <omaymaelmrhasli@gmail.com>
2023-08-08 16:49:34 -0700

    if,else

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for xup in range(ord('a'), ord('b') + 1):
-    if chr(xup) != 'q' and chr(xup) != 'e':
-        print("{}".format(chr(xup)), end='')
+# Author - Tolulope Fakunle
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
