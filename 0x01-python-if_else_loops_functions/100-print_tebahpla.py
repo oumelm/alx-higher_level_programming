6aac4a061d3c6d7103ec05b001323f6ce0ca545b
oumelm <omaymaelmrhasli@gmail.com>
2023-08-08 16:35:18 -0700

    if,else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 # Author - Bamidele Adefolaju
 
 i = 0
-for c in range('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
