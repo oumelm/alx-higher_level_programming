14204be40f3e0b9fffa24dd6db28aaf8a7f7098b
oumelm <omaymaelmrhasli@gmail.com>
2023-08-12 00:01:16 +0000

    I

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Print the number of and the list of its arguments
+if __name__ == "__main__":
+    import sys
+
+    arg = sys.argv
+    sizee = len(arg) - 1
+
+    if size > 1:
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i]))
+    elif size == 0:
+        print("{} arguments.".format(size))
+
+    else:
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
