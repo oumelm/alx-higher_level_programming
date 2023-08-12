df8119ba2a5745c4f4ee2a01d8ccbeb3496c3e4c
oumelm <omaymaelmrhasli@gmail.com>
2023-08-12 00:33:44 +0000

    I

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,13 @@ if __name__ == "__main__":
     import sys
 
     arg = sys.argv
-    sizee = len(arg) - 1
+    size = len(arg) - 1
 
     if size > 1:
         print("{} arguments:".format(size))
         for i in range(1, size + 1):
             print("{}: {}".format(i, arg[i]))
+
     elif size == 0:
         print("{} arguments.".format(size))
 
