5a0802df00d5d997c39ac7d346f7688941d0512b
oumelm <omaymaelmrhasli@gmail.com>
2023-08-28 19:05:13 +0000

    E

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             c += 1
-         except (ValueError, TypeError):
-             pass
-         i += 1
-     print()
-     return c
+        except (ValueError, TypeError):
+            pass
+        i += 1
+    print()
+    return c
