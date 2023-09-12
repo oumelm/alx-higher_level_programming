b8f7988dbe8ef87b24d3cb482e1f34d707edc18d
oumelm <omaymaelmrhasli@gmail.com>
2023-09-12 00:24:23 +0000

    I

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""Defines a class-checking function."""
+"""Defines an inherited list class MyList."""
 
 
-def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
 
-    if type(obj) == a_class:
-        return True
-
-    return False
+        print(sorted(self))
