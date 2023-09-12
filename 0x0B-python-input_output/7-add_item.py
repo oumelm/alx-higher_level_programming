7506563bd6bd87b218f3a7f14f797807e64422a1
oumelm <omaymaelmrhasli@gmail.com>
2023-09-12 23:40:20 +0000

    O

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arglist = list(sys.argv[1:])
 
